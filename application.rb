c11a09508c3d81b40d88184605123c41a424455a
David A McClain <david@uhhuhyeah.com>
2010-11-15 12:25:34 -0800

    initial commit

---
+++ application.rb
@@ -0,0 +1,48 @@
+require 'rubygems'
+require 'sinatra'
+require 'erb'
+require 'sqlite3'
+require 'dm-core'
+require 'dm-migrations'
+
+configure :development do
+  ## Run
+  # sqlite3 test.db "create table mailing_lists (id INTEGER PRIMARY KEY, email STRING, created_at DATETIME);"
+  
+  DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/test.db")
+  DataMapper.auto_upgrade!
+end
+
+configure :production do
+  ## ENV['DATABASE_URL'] is provided by heroku for connecting to their Postgres db
+  ## See this for more info - http://docs.heroku.com/database#using-the-databaseurl-environment-variable-sequel--datamapper
+  DataMapper::setup(:default, ENV['DATABASE_URL'] || 'sqlite3://application.db')
+end
+
+class MailingList
+  include DataMapper::Resource
+  property :id,         Serial
+  property :email,      String, :length => 150
+  property :created_at, DateTime
+end
+
+DataMapper.finalize
+
+get '/' do
+  @list = MailingList.all.count
+  erb :index
+end
+
+get '/thanks' do
+  erb :thanks
+end
+
+post '/subscribe' do
+  email = params[:subscribe][:email]
+  
+  unless email.blank? || email == 'Enter your email address'
+    MailingList.create(:email => email, :created_at => Time.now.utc)
+  end
+  
+  redirect '/thanks'
+end
